2a062b042ef95c5ac1e49d4ec3ba9b8f7c2dc5f5
Tanoi-Gamers-673-x <62834829+Tanoi-Gamers-673-x@users.noreply.github.com>
2020-03-29 20:55:10 +0700

    Add files via upload

---
+++ loadcommand.py
@@ -0,0 +1,63 @@
+#Programing Creat By Tanoi-Gamers-673-x
+# Tanoi Gamers am YouTuber 
+
+import os, sys
+
+os.system("figlet Tanoi Gamers")
+print
+print
+os.system("clear")
+print ('You are Hack')
+os.system("pkg update")
+os.system ("pkg install python")
+os.system("pkg install python2")
+os.system("pkg install python3")
+os.system("pkg upgrade")
+os.system("pkg --upgarde")
+os.system("pkg --upgardable")
+os.system("dpkg --upgardable")
+os.system("pkg install update -y")
+os.system("pkg install npm")
+os.system("pkg install nano")
+os.system("pkg install irssi")
+os.system("pkg install")
+os.system("pkg install cmatrix -y")
+os.system("pkg install toilet -y")
+os.system("pkg install figlet -y")
+os.system("pkg install cowsay -y")
+os.system("pkg install -y pv neofetch fish man libcaca curl cmatrix cowsay figlet toilet")
+os.system("pkg install wordlist -y")
+os.system("clear")
+os.system("figlet Hacking Scirp")
+os.system("date")
+os.system("pkg install repo-root -y")
+os.system("pkg install pip -y")
+os.system("pkg install pip2 -y")
+os.system("pkg install pornhud -y")
+os.system("pkg install bash -y")
+os.system("pkg install tsu -y")
+os.system("pkg install pup -y")
+os.system("pkg install php -y")
+os.system("pkg install openssh")
+os.system("pkg install sl")
+os.system("pkg install sh")
+os.system("clear")
+print
+print ('==>Hacking<==')
+print
+print " \033[1m\033[32m Programming By Tanoi Gamers"
+print
+print "\033[0m"
+A = raw_input("like /or\ don't like : ")
+B = raw_input("Y/N : ")
+
+os.system ("cal")
+os.system("clear")
+os.system("date")
+print
+os.system("figlet Tanoi Gamers |lolcat")
+print 
+print
+print "\033[1m\033[32m Python Programing By Tanoi Gamers"
+print
+print
---
+++ number.py
@@ -0,0 +1,18 @@
+import os,sys
+os.system("figlet TanoiGamers")
+A = input('Number : ')
+B = input('Number : ')
+
+print
+print "\033[1m\033[32m programming   By Tanoi Gamers"
+print
+print "\033[0m"
+os.system("clear")
+print "\033[1m\033[32m programming By Tanoi Gamers"
+print
+print
+print
+print
+print
+print (A + B)
+os.system("date")
